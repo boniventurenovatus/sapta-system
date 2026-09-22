61d36e2d9ea75be1bd5ea8bc333770b8cb683044
boniventurenovatus <boniventurenovatus@gmail.com>
2026-09-23 01:14:36 +0300

    Rudisha message kwenye saptaFormConfirm - Suspend/Deactivate/Terminate

--- resources/views/permissions/show.blade.php
+++ resources/views/permissions/show.blade.php
@@ -131,7 +131,7 @@
                     <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
                     Edit
                 </a>
-                <form method="POST" action="{{ route('permissions.destroy', $permission) }}" class="inline" onsubmit="return saptaFormConfirm(event, '')">
+                <form method="POST" action="{{ route('permissions.destroy', $permission) }}" class="inline" onsubmit="return saptaFormConfirm(event, 'Are you sure?')">
                     @csrf @method('DELETE')
                     <button type="submit" class="btn-delete">
                         <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
