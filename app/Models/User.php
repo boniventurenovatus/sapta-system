4fc503c8089ff5564b3ad64caa53c5b7be979518
boniventurenovatus <boniventurenovatus@gmail.com>
2026-09-22 20:36:00 +0300

    Fix User model duplicate setPasswordAttribute + verify syntax

--- app/Models/User.php
+++ app/Models/User.php
@@ -148,6 +148,9 @@ class User extends Authenticatable
         } else {
             $this->attributes['password_hash'] = Hash::make($value);
         }
+    } else {
+            $this->attributes['password_hash'] = Hash::make($value);
+        }
     }
 
     /*
