2cbeedcb144e3a04842ea5c98d7c36476aead5a9
boniventurenovatus <boniventurenovatus@gmail.com>
2026-09-25 18:25:57 +0300

    Ondoa left-side + weka logo juu ya login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -770,6 +770,15 @@
                 <div class="login-brand-name">SAPTA</div>
                 <div class="login-brand-tagline">Quality Agriculture Work</div>
 
+                <div class="login-logo-wrap">
+                    <div class="login-logo">
+                        <img src="{{ asset('images/sapta-logo.png') }}" alt="SAPTA Logo">
+                    </div>
+                </div>
+
+                <div class="login-brand-name">SAPTA</div>
+                <div class="login-brand-tagline">Quality Agriculture Work</div>
+
                 <div class="eyebrow">
                     QUALITY AGRICULTURE WORK
                 </div>
